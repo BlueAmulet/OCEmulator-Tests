ca0c016fdd0cae592c06d5d390e2802a4cac8345
gamax92 <gamax92@aol.com>
2016-12-03 23:36:16 -0700

    More os.date tests
    Test behaviour of garbage arguments
    Test for truncation at NUL character
    Test for default format string
    
    Remove isdst from tzoffset calculation

--- tests/os.lua
+++ tests/os.lua
@@ -2,16 +2,16 @@ local test = require("test")
 
 -- os.time tests
 
--- Check that isdst does not work
+-- isdst is not supported
 test.evaluate(os.time({day=1,month=1,year=1970,isdst=false}) == os.time({day=1,month=1,year=1970,isdst=true}))
 
 -- os.date tests
 
--- Invalid options don't error
+-- Invalid format options don't error
 test.shouldNotError(os.date, "%i")
 
 -- Verify valid options format correctly
-local tzoffset = os.time({year=1970,day=1,wday=5,isdst=false,sec=0,yday=1,month=1,min=0,hour=0})
+local tzoffset = os.time({year=1970,day=1,wday=5,sec=0,yday=1,month=1,min=0,hour=0})
 local testtime = 826405092 + tzoffset
 test.evaluate(os.date("%%") == "%")
 test.evaluate(os.date("%A", testtime) == "Saturday")
@@ -43,6 +43,27 @@ test.evaluate(os.date("%w", testtime) == "6")
 test.evaluate(os.date("%x", testtime) == "03/09/96")
 test.evaluate(os.date("%y", testtime) == "96")
 
+-- Garbage arguments do not error
+test.shouldNotError(os.date, nil, testtime)
+test.shouldNotError(os.date, {}, testtime)
+test.shouldNotError(os.date, true, testtime)
+test.shouldNotError(os.date, print, testtime)
+
+test.shouldNotError(os.date, "%c", "asdf")
+test.shouldNotError(os.date, "%c", nil)
+test.shouldNotError(os.date, "%c", {})
+test.shouldNotError(os.date, "%c", true)
+test.shouldNotError(os.date, "%c", print)
+
+-- Verify default formatting
+test.evaluate(os.date(nil, testtime) == "09/03/96 20:58:12")
+
+-- Numbers are accepted as formatting strings
+test.evaluate(os.date(1234) == "1234")
+
+-- Strings are accepted as time
+test.evaluate(os.date("%Y-%m-%d %H:%M:%S", tostring(testtime)) == "1996-03-09 20:58:12")
+
 -- Non valid options should all replace with empty strings
 local nonvalid = "EGJKLNOPQUVWZfgikloqsuvz"
 for i=1, #nonvalid do
@@ -52,9 +73,11 @@ end
 -- Test bugs
 test.evaluate(os.date("%e", os.time({day=10,month=1,year=1970})) == " 10")
 
--- Check that timezone modifier does not work
+-- Timezone modifier does not work
 test.evaluate(os.date("%c", testtime) == os.date("!%c", testtime))
 
 -- Check that isdst is not returned
-test.evaluate(os.date("*t").isdst==nil)
+test.evaluate(os.date("*t").isdst == nil)
 
+-- Check for truncation at NUL
+test.evaluate(os.date("abc\0def") == "abc")
