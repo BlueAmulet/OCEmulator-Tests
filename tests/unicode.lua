de9957e733c8efead7b420ec26e98c8abac027b9
gamax92 <gamax92@aol.com>
2017-05-27 16:30:09 -0600

    Add tests regarding zero width characters

--- tests/unicode.lua
+++ tests/unicode.lua
@@ -2,6 +2,7 @@ local test = require("test")
 local unicode = require("unicode")
 
 local wide = unicode.char(0x3000) -- Nothing special about this character
+local zwidth = unicode.char(0xFFFF) -- Character with zero width
 
 -- Character tests
 test.evaluate("", unicode.char)
@@ -21,12 +22,14 @@ test.evaluate(2, unicode.charWidth, wide)
 test.evaluate(1, unicode.charWidth, "x" .. wide)
 test.evaluate(2, unicode.charWidth, wide .. "x")
 test.evaluate(1, unicode.charWidth, "\0")
+test.evaluate(0, unicode.charWidth, zwidth)
 
 test.evaluate(false, unicode.isWide, "x")
 test.evaluate(true, unicode.isWide, wide)
 test.evaluate(false, unicode.isWide, "x" .. wide)
 test.evaluate(true, unicode.isWide, wide .. "x")
 test.evaluate(false, unicode.isWide, "\0")
+test.evaluate(false, unicode.isWide, zwidth)
 
 -- Unicode length tests
 test.evaluate(3, unicode.len, string.rep("X",3))
@@ -39,6 +42,7 @@ test.evaluate(3, unicode.wlen, string.rep("X",3))
 test.evaluate(6, unicode.wlen, string.rep(wide,3))
 test.evaluate(9, unicode.wlen, string.rep("X" .. wide,3))
 test.evaluate(7, unicode.wlen, "RATED\0X")
+test.evaluate(1, unicode.wlen, zwidth)
 
 -- Unicode truncation tests
 test.evaluate("", unicode.wtrunc, "ABCDEFG",-7)
@@ -58,6 +62,16 @@ test.evaluate(string.rep(wide,1), unicode.wtrunc, widestr,4)
 test.evaluate(string.rep(wide,2), unicode.wtrunc, widestr,5)
 test.evaluate(string.rep(wide,2), unicode.wtrunc, widestr,6)
 test.shouldError(unicode.wtrunc, widestr, 7)
+
+local zwidthstr = string.rep(zwidth, 5)
+test.evaluate("", unicode.wtrunc, zwidthstr, 0)
+test.shouldError(unicode.wtrunc, zwidthstr, 1)
+test.evaluate("", unicode.wtrunc, "a" .. zwidthstr, 1)
+test.shouldError(unicode.wtrunc, "a" .. zwidthstr, 2)
+test.evaluate(zwidthstr, unicode.wtrunc, zwidthstr .. "b", 1)
+test.shouldError(unicode.wtrunc, zwidthstr .. "b", 2)
+test.evaluate("a" .. zwidthstr, unicode.wtrunc, "a" .. zwidthstr .. "b", 2)
+test.shouldError(unicode.wtrunc, "a" .. zwidthstr .. "b", 3)
 
 -- Unicode operation tests
 test.evaluate("abcγγγabcγγγ", unicode.lower, "ABCΓΓΓabcγγγ")
